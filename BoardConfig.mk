ff6496ca2a81e3c3ba5b262a2c94d4fc3dbe39cd
electrikjesus <electrikjesus@gmail.com>
2015-06-16 10:30:40 -0400

    Blissified the boardconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,9 +20,27 @@
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 TARGET_KERNEL_CONFIG := cyanogenmod_n8013_defconfig
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 
 # assert
 TARGET_OTA_ASSERT_DEVICE := c0,p4notewifi,p4notewifiww,n8013,GT-N8013,n8010,GT-N8010
 
 #Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/n8013/bluetooth
+
+# BlissPop Configs
+BLISS_WIPE_CACHES := 1
+TARGET_TC_ROM := 4.8
+TARGET_TC_KERNEL := 4.8
+BLISSIFY := true
+BLISS_O3 := true
+BLISS_STRICT := false
+BLISS_GRAPHITE := true
+BLISS_KRAIT := false
+BLISS_PIPE := true
+FLOOP_NEST_OPTIMIZE := true
+ENABLE_GCCONLY := true
+TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+#SaberMod
+-include vendor/bliss/config/sm.mk
